f4d668f5f008510451167995ed2d67135d6d85b9
van <ivan.agbay@evsu.edu.ph>
2025-11-14 21:10:22 +0800

    Improve input handling for message and options
    Refactor message and options handling based on input type.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
-﻿using System;
-using System.Linq;
-
 class Program
 {
     static void Main(string[] args)
@@ -10,10 +7,36 @@ class Program
         // Use piped input or message from args
         string message = Console.IsInputRedirected
             ? Console.In.ReadToEnd().Trim()
-            : string.Join(" ", args.Where(a => !a.StartsWith("-")));
+            : "";
 
-        // Get cowsay options (flags)
-        var options = args.Where(a => a.StartsWith("-")).ToArray();
+        // Everything from args goes to options if we have piped input
+        // Otherwise, separate message from options
+        string[] options;
+        
+        if (Console.IsInputRedirected)
+        {
+            // All args are options when input is piped
+            options = args;
+        }
+        else
+        {
+            // Find where options start (first arg starting with -)
+            int firstOptionIndex = Array.FindIndex(args, a => a.StartsWith("-"));
+            
+            if (firstOptionIndex == -1)
+            {
+                // No options, all args are the message
+                message = string.Join(" ", args);
+                options = Array.Empty<string>();
+            }
+            else
+            {
+                // Everything before first option is message
+                message = string.Join(" ", args.Take(firstOptionIndex));
+                // Everything from first option onwards are options
+                options = args.Skip(firstOptionIndex).ToArray();
+            }
+        }
 
         Console.Write(cowsay.Run(message, options));
     }
